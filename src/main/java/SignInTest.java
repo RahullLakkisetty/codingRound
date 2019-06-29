c9a3f2ed55e8e233e0e83be61b8fc75d86d3795e
RahullLakkisetty <52246813+RahullLakkisetty@users.noreply.github.com>
2019-06-29 18:20:07 +0530

    UpdatedSignInTest.jaa
    Updated Test page with TestNG annotations, created UtlitiyBase class which implements all functionalities of SignIn class

--- src/main/java/SignInTest.java
+++ src/main/java/SignInTest.java
@@ -1,8 +1,13 @@
+import java.io.IOException;
 import com.sun.javafx.PlatformUtil;
-import org.openqa.selenium.By;
+import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 public class SignInTest {
@@ -22,45 +27,38 @@ public class SignInTest {
 	    @FindBy(id = "errors1")
 	    private WebElement errorMessage;
 
-    WebDriver driver = new ChromeDriver();
+        /*Intialize WebDriver from UtilityBase*/
+	@BeforeSuite	
+	public void intializeDriver() throws IOException {
 
-    @Test
-    public void shouldThrowAnErrorIfSignInDetailsAreMissing() {
+	driver=UtilityBase.getWebDriver();
+	driver.get("https://www.cleartrip.com");
+	driver.manage().window().maximize();
+	driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+	PageFactory.initElements(driver, this);
 
-        setDriverPath();
 
-        driver.get("https://www.cleartrip.com/");
-        waitFor(2000);
+	}
 
-        driver.findElement(By.linkText("Your trips")).click();
-        driver.findElement(By.id("SignIn")).click();
+        @Test
+	public void shouldThrowAnErrorIfSignInDetailsAreMissing()
+	{
+	clickButton(yourTrips);
+	clickButton(signIn);
+	iFrame(modalWindow,driver);
+	clickButton(frameSignIn);
+	String errorMsg=getText(errorMessage);
+	Assert.assertTrue(errorMsg.contains("There were errors in your submission"));	
+	}
+	
+	@AfterSuite
+	public void quitBrowser(){
+	//To Close the web browser at the end of test.
+	{
+	driver.quit();
+	}
 
-        driver.findElement(By.id("signInButton")).click();
-
-        String errors1 = driver.findElement(By.id("errors1")).getText();
-        Assert.assertTrue(errors1.contains("There were errors in your submission"));
-        driver.quit();
-    }
-
-    private void waitFor(int durationInMilliSeconds) {
-        try {
-            Thread.sleep(durationInMilliSeconds);
-        } catch (InterruptedException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
-    }
-
-    private void setDriverPath() {
-        if (PlatformUtil.isMac()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver");
-        }
-        if (PlatformUtil.isWindows()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        }
-        if (PlatformUtil.isLinux()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver_linux");
-        }
-    }
+   
 
 
 }
