53477bbc0c8cd2eebddc756014466b7f1a22d5e4
RahullLakkisetty <52246813+RahullLakkisetty@users.noreply.github.com>
2019-06-29 21:02:10 +0530

    Update SignInTest.java

--- src/main/java/SignInTest.java
+++ src/main/java/SignInTest.java
@@ -11,8 +11,10 @@ import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 public class SignInTest {
+	
+	    private static WebDriver driver=null;
     
-    	@FindBy(xpath = "//*[@id='userAccountLink']")
+    	    @FindBy(xpath = "//*[@id='userAccountLink']")
 	    private WebElement yourTrips;
 	    
 	    @FindBy(xpath = "//*[@id='SignIn']")
@@ -26,39 +28,37 @@ public class SignInTest {
 	    
 	    @FindBy(id = "errors1")
 	    private WebElement errorMessage;
-
-        /*Intialize WebDriver from UtilityBase*/
-	@BeforeSuite	
-	public void intializeDriver() throws IOException {
-
-	driver=UtilityBase.getWebDriver();
-	driver.get("https://www.cleartrip.com");
-	driver.manage().window().maximize();
-	driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
-	PageFactory.initElements(driver, this);
-
-
-	}
-
-        @Test
-	public void shouldThrowAnErrorIfSignInDetailsAreMissing()
-	{
-	clickButton(yourTrips);
-	clickButton(signIn);
-	iFrame(modalWindow,driver);
-	clickButton(frameSignIn);
-	String errorMsg=getText(errorMessage);
-	Assert.assertTrue(errorMsg.contains("There were errors in your submission"));	
-	}
 	
-	@AfterSuite
-	public void quitBrowser(){
-	//To Close the web browser at the end of test.
-	{
-	driver.quit();
-	}
+	
 
-   
+            /*Intialize WebDriver from UtilityBase*/
+	    @BeforeSuite	
+	    public void intializeDriver() throws IOException 
+	    {
+	    driver=UtilityBase.getWebDriver();
+	    driver.get("https://www.cleartrip.com");
+	    driver.manage().window().maximize();
+	    driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+	    PageFactory.initElements(driver, this);
+	    }
 
+	
+            @Test
+	    public void shouldThrowAnErrorIfSignInDetailsAreMissing()
+	    {
+	    clickButton(yourTrips);
+	    clickButton(signIn);
+	    iFrame(modalWindow,driver);
+	    clickButton(frameSignIn);
+	    String errorMsg=getText(errorMessage);
+	    Assert.assertTrue(errorMsg.contains("There were errors in your submission"));	
+	    }
+	
+	
+	    @AfterSuite
+	    public void quitBrowser()
+	    {
+	    driver.quit();
+	    }
 
 }
